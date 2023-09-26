a05bf193132ea578a002d4cfb412305356102038
ocastelblack <oscard.castelblanco@gmail.com>
2023-09-26 01:17:11 -0500

    cambios

--- edit_web_payment/__manifest__.py
+++ edit_web_payment/__manifest__.py
@@ -1,9 +1,11 @@
 {
-    'name': 'Personalizar Progress Wizard',
+    'name': 'customize client form web page',
     'version': '1.0',
     'category': 'Website',
-    'summary': 'Personalización del Progress Wizard',
-    'description': 'Agrega un div y un span después del progress-wizard.',
+    'summary': 'customize client form web page',
+    'description': 'The integration of the company and individual radios is carried out, the identification type drop-down list.',
+    'author': 'Oscar Castelblanco,Sakya',
+    'website': 'https://www.sakya.co/',
     'depends': ['base','website','website_sale'],
     'data': [
         'views/wizard_template.xml',  # Archivo XML con la vista personalizada
--- edit_web_payment/models/web_client_data_edit.py
+++ edit_web_payment/models/web_client_data_edit.py
@@ -11,13 +11,25 @@ class WebsiteSalePruebas(models.Model):
         return 'Hola'
     
     def extract_identification_type_number(self,checkout):
-        identification_type_id = str(checkout) 
-        match = re.search(r'\((\d+),\)', identification_type_id)
+        # identification_type_id = str(checkout) 
+        # match = re.search(r'\((\d+),\)', identification_type_id)
 
-        if match:
-            extracted_number = match.group(1)
+        # if match:
+        #     extracted_number = match.group(1)
+        # else:
+        #     extracted_number = None
+
+        # return extracted_number
+    
+        if checkout:
+            identification_type_id = str(checkout)
+            match = re.search(r'\((\d+),\)', identification_type_id)
+            if match:
+                extracted_number = match.group(1)
+            else:
+                extracted_number = 1  # Set the default value to 1 if checkout is not provided or is empty
         else:
-            extracted_number = None
+            extracted_number = 1  # Set the default value to 1 if checkout is not provided or is empty
 
         return extracted_number
     
@@ -28,13 +40,14 @@ class WebsiteSalePruebas(models.Model):
 class CustomWebsiteSale(WebsiteSale):
     @http.route(['/shop/address'], type='http', methods=['GET', 'POST'], auth="public", website=True, sitemap=False)
     def address(self, **kw):
+        is_company = False
         identification_type_id_str = kw.get('identification_type_id')
         if identification_type_id_str:
             l10n_latam_identification_type_id = int(identification_type_id_str)
         else:
             l10n_latam_identification_type_id = False
             
-        is_company = kw.get('is_company')
+        is_company_str = kw.get('radio_field')
         
         if l10n_latam_identification_type_id:
             order = request.website.sale_get_order()
@@ -43,8 +56,13 @@ class CustomWebsiteSale(WebsiteSale):
             if partner_id:
                 partner = request.env['res.partner'].sudo().browse(partner_id)
                 partner.write({'l10n_latam_identification_type_id': l10n_latam_identification_type_id})
-
-        if is_company:
+        
+        if is_company_str:
+            if is_company_str=='individual':
+                is_company= False
+            else:
+                is_company= True
+                
             if partner_id:
                 partner = request.env['res.partner'].sudo().browse(partner_id)
                 partner.write({'is_company': is_company})
