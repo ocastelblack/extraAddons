d0dfd8fbd6afe942c2d235c29b15192f61d07c26
ocastelblack <oscard.castelblanco@gmail.com>
2023-10-19 15:10:49 -0500

    cambios

--- custom_inventory_pruebas/models/product_item.py
+++ custom_inventory_pruebas/models/product_item.py
@@ -4,9 +4,11 @@ class CustomProductItem(models.Model):
     _inherit = 'product.product'
 
     stock_miami = fields.Integer(string='stock_miami')
-    last_updated = fields.Datetime(string='Última Actualización stock_miami', compute="compute_custom_field", store=True)
+    last_updated = fields.Datetime(string='Última Actualización stock_miami', store=True, readonly=False)
+    # last_updated = fields.Datetime(string='Última Actualización stock_miami', compute="compute_custom_field", store=True,
+    #     readonly=False)
     
-    @api.depends('stock_miami')
-    def compute_custom_field(self):
-        for record in self:
-            record.last_updated = fields.Datetime.now()
+    # @api.depends('stock_miami')
+    # def compute_custom_field(self):
+    #     for record in self:
+    #         record.last_updated = fields.Datetime.now()
